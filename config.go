9098903b9bd3a39ae869d8d12f8ae530b35d6632
Ab1659 <ab1659@wildcats.unh.edu>
2021-10-05 12:54:24 -0400

    Creating config, db and web

---
+++ config.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"fmt"
+	"hash/fnv"
+)
+
+package config
+
+import (
+"fmt"
+"hash/fnv"
+
+"github.com/BurntSushi/toml"
+)
+
+type Shard struct {
+	Name    string
+	Idx     int
+	Address string
+}
+
+type Config struct {
+	Shards []Shard
+}
+
+type Shards struct {
+	Count   int
+	CurrIdx int
+	Addrs   map[int]string
+}
+
+func ParseFile(fileName string) (Config, error) {
+	var c Config
+	_, err := toml.DecodeFile(fileName, &c)
+	if err != nil {
+		return Config{}, err
+	}
+	return c, nil
+}
+
+func ParseShards(shards []Shard, currShardName string) (*Shards, error) {
+	var shardCount = len(shards)
+	var shardIdx int = -1
+	var addrs = make(map[int]string)
+	for _, s := range shards {
+
+		if _, ok := addrs[s.Idx]; ok {
+			return nil, fmt.Errorf("duplicate shard index %q found", s.Idx)
+		}
+
+		addrs[s.Idx] = s.Address
+		if s.Name == currShardName {
+			shardIdx = s.Idx
+		}
+	}
+
+	for i := 0; i < shardCount; i++ {
+		if _, ok := addrs[i]; !ok {
+			return nil, fmt.Errorf("shard index %q not found", i)
+		}
+	}
+
+	if shardIdx < 0 {
+		return nil, fmt.Errorf("shard %s not found", currShardName)
+	}
+
+	return &Shards{
+		Count:   shardCount,
+		CurrIdx: shardIdx,
+		Addrs:   addrs,
+	}, nil
+}
+
+func (s *Shards) Index(key string) int {
+	h := fnv.New64()
+	h.Write([]byte(key))
+	return int(h.Sum64() % uint64(s.Count))
+}
---
+++ web.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+
+	"/Users/abishek/GolandProjects/awesomeProject1/config"
+	"/Users/abishek/GolandProjects/awesomeProject1/db"
+)
+
+type Server struct {
+	db     *db.Database
+	shards *config.Shards
+}
+
+func NewServer(db *db.Database, s *config.Shards) *Server {
+	return &Server{
+		db:     db,
+		shards: s,
+	}
+}
+
+func (s *Server) redirect(shard int, rw http.ResponseWriter, r *http.Request) {
+
+	fmt.Fprintf(rw, "redirecting from shard %d to shard %d\n",
+		s.shards.CurrIdx, shard)
+
+	url := "http://" + s.shards.Addrs[shard] + r.RequestURI
+	resp, err := http.Get(url)
+	if err != nil {
+		rw.WriteHeader(500)
+		fmt.Fprintf(rw, "failed to redirect request: %v", err)
+		return
+	}
+
+	defer resp.Body.Close()
+	io.Copy(rw, resp.Body)
+}
+
+func (s *Server) GetHandler(rw http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	key := r.Form.Get("key")
+
+	shard := s.shards.Index(key)
+	if shard != s.shards.CurrIdx {
+		s.redirect(shard, rw, r)
+		return
+	}
+
+	value, err := s.db.GetKey(key)
+	fmt.Fprintf(rw, "shard = %d curr shard = %d Value = %q, error = %v\n",
+		shard, s.shards.CurrIdx, value, err)
+}
+
+func (s *Server) SetHandler(rw http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	key := r.Form.Get("key")
+	value := r.Form.Get("value")
+
+	shard := s.shards.Index(key)
+	if shard != s.shards.CurrIdx {
+		s.redirect(shard, rw, r)
+		return
+	}
+
+	err := s.db.SetKey(key, []byte(value))
+	fmt.Fprintf(rw, "shard = %d curr shard = %d error = %v\n",
+		shard, s.shards.CurrIdx, err)
+}
+
+func (s *Server) DeleteExtraKeysHandler(rw http.ResponseWriter, r *http.Request) {
+
+	fmt.Fprintf(rw, "error cleaning extra keys: %v",
+		s.db.DeleteExtraKeys(func(key string) bool {
+			return s.shards.Index(key) != s.shards.CurrIdx
+		}))
+
+}
