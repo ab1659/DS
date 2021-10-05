3a1395e242725d2fa80dbcf8a62fd0da74e7b20c
ab1659 <90533768+ab1659@users.noreply.github.com>
2021-10-05 13:01:21 -0400

    Add files via upload

---
+++ db.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"fmt"
+
+	bolt "go.etcd.io/bbolt"
+)
+
+var (
+	defaultBucket = []byte("default")
+)
+
+// Database ....
+type Database struct {
+	db *bolt.DB
+}
+
+func NewDatabase(dbPath string) (db *Database, closeFunc func() error, err error) {
+	boltDB, err := bolt.Open(dbPath, 0600, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	db = &Database{db: boltDB}
+	if err := db.createDefaultBucket(); err != nil {
+		db.db.Close()
+		return nil, nil, fmt.Errorf("failed to create bucket: %v", err)
+	}
+
+	return db, boltDB.Close, nil
+}
+
+func (d *Database) createDefaultBucket() error {
+	return d.db.Update(func(t *bolt.Tx) error {
+		_, err := t.CreateBucketIfNotExists(defaultBucket)
+		return err
+	})
+}
+
+func (d *Database) SetKey(key string, value []byte) error {
+	err := d.db.Update(func(t *bolt.Tx) error {
+		b := t.Bucket(defaultBucket)
+		return b.Put([]byte(key), value)
+	})
+	return err
+}
+
+func (d *Database) GetKey(key string) (value []byte, err error) {
+	err = d.db.View(func(t *bolt.Tx) error {
+		b := t.Bucket(defaultBucket)
+		value = b.Get([]byte(key))
+		if value == nil {
+			return fmt.Errorf("key %s not found", key)
+		}
+		return nil
+	})
+	return value, err
+}
+
+func (d *Database) DeleteExtraKeys(isExtra func(string) bool) error {
+	var keys []string
+
+	err := d.db.View(func(t *bolt.Tx) error {
+		b := t.Bucket(defaultBucket)
+		return b.ForEach(func(k, v []byte) error {
+			ks := string(k)
+			if isExtra(ks) {
+				keys = append(keys, ks)
+			}
+			return nil
+		})
+
+	})
+	if err != nil {
+		return err
+	}
+
+	return d.db.Update(func(t *bolt.Tx) error {
+		b := t.Bucket(defaultBucket)
+		for _, k := range keys {
+			if err := b.Delete([]byte(k)); err != nil {
+				return err
+			}
+		}
+		return nil
+	})
+
+}
